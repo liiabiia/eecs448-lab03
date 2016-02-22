02514529ae4f3c6566c4643a8b477545650830f9
Liia Butler <l173b625@ku.edu>
2016-02-22 14:21:10 -0600

    Add final touches to second activity

--- eecs448-lab03/Exercise_2/SlideShow.js
+++ eecs448-lab03/Exercise_2/SlideShow.js
@@ -6,23 +6,23 @@ var photo = 1;
 var next = function(){
 
     if(photo == 1){
-    
+		document.getElementById("screenshot").src="Screenshot_2015-03-20-15-50-19.png";
         photo = 2;
         //change source pic
     } else if(photo == 2) {
-    
+		document.getElementById("screenshot").src= "Screenshot_2015-03-20-15-51-36.png";
         photo = 3;
         //change source pic
     } else if(photo == 3){
-    
+		document.getElementById("screenshot").src= "Screenshot_2015-03-20-15-54-44.png";
         photo = 4;
         //change source pic
     } else if(photo == 4){
-    
+		document.getElementById("screenshot").src= "Screenshot_2015-09-27-09-11-57.png";
         photo = 5;
         //change source pic
     } else{
-    
+		document.getElementById("screenshot").src="Screenshot_2015-03-19-16-35-35.png";
         photo = 1;
         //change source pic
     }
@@ -30,23 +30,23 @@ var next = function(){
 var previous = function(){
 
     if(photo == 1){
-    
+		document.getElementById("screenshot").src= "Screenshot_2015-09-27-09-11-57.png";
         photo = 5;
         //change source pic
     } else if(photo == 2) {
-    
+		document.getElementById("screenshot").src="Screenshot_2015-03-19-16-35-35.png";
         photo = 1;
         //change source pic
     } else if(photo == 3){
-    
+		document.getElementById("screenshot").src= "Screenshot_2015-03-20-15-50-19.png";
         photo = 2;
         //change source pic
     } else if(photo == 4){
-    
+		document.getElementById("screenshot").src= "Screenshot_2015-03-20-15-51-36.png";
         photo = 3;
         //change source pic
     } else{
-    
+		document.getElementById("screenshot").src= "Screenshot_2015-03-20-15-54-44.png";
         photo = 4;
         //change source pic
     }
