0eb4bc094d555802254f3aabf834ec4c2f1c7692
Liia Butler <l173b625@ku.edu>
2016-02-22 10:14:09 -0600

    Add button logic to make index html actually work

--- eecs448-lab03/Exercise_1/password.js
+++ eecs448-lab03/Exercise_1/password.js
@@ -1,4 +1,15 @@
-var validate = function(){
-
-    document.getElementById("
+function validate(form){
+    var pass1Obj = document.getElementById("Pass1");
+    var pass1 = pass1Obj.form.Password1.value;
+    var pass2Obj = document.getElementById("Pass2");
+    var pass2 = pass2Obj.form.Password2.value;
+    if((pass1.length < 8) || (pass2.length < 8)){
+        alert("ERROR: Password length must be greater than 8 characters.");
+    } else if (pass1 != pass2){
+    
+        alert("ERROR: Passwords do not match.");
+    } else {
+    
+        alert("Match!");
+    }
 }
